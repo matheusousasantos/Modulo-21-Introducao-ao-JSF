b5fb08c128c88ad7ddca58a5e246798b0e3ce71f
Matheus Sousa <matheus.ads.study@gmail.com>
2019-07-30 14:10:05 -0300

    #08 - Testando o JPA e a criação de tabelas

---
+++ src/test/java/br/com/cursojsf/TesteJPA.java
@@ -0,0 +1,13 @@
+package br.com.cursojsf;
+
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+	
+	public static void main(String[] args) {
+		
+		Persistence.createEntityManagerFactory("meuprimeiroprojetojsf");
+		
+	}
+
+}
