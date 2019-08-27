573a8b8de0d61f9b20fffb41a88b897a8d542674
Matheus Sousa <matheus.ads.study@gmail.com>
2019-08-27 12:52:30 -0300

    # 10 - Filter para autenticação de usuário logado & # 11- CORREÇÃO - Explicando sofre o filtro de usuário

---
+++ src/main/java/br/com/filter/FilterAutenticacao.java
@@ -0,0 +1,58 @@
+package br.com.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.entidades.Pessoa;
+import br.com.jppautil.JPAUtil;
+
+@WebFilter(urlPatterns = {"/*"})
+public class FilterAutenticacao implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		Pessoa pessoaLogada = (Pessoa) session.getAttribute("pessoaLogado");
+		
+		String url = req.getServletPath();
+		
+		if(!url.equalsIgnoreCase("index.jsf") && pessoaLogada == null) {
+			
+			RequestDispatcher view = request.getRequestDispatcher("/index.jsf");
+			view.forward(request, response);
+			return;
+			
+		} else {
+		
+			chain.doFilter(request, response);
+		
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		JPAUtil.getEntityManager();
+	}
+
+}
