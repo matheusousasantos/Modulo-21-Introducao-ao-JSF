3ad01e98e97684dedcc419f063d8b308e2130848
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-02 10:29:58 -0300

    Módulo 28 - Aprofundando em JSF e JPA

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,5 +1,10 @@
 package br.com.cursojsf;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,28 +17,29 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.facelets.FaceletContext;
 
+import org.apache.jasper.tagplugins.jstl.core.Url;
+
 import br.com.dao.DaoGeneric;
 import br.com.entidades.Pessoa;
 import br.com.repository.IDaoPessoa;
 import br.com.repository.IDaoPessoaImpl;
 
-
-@ManagedBean( name = "pessoaBean" )
+@ManagedBean(name = "pessoaBean")
 @ViewScoped
 public class PessoaBean {
-	
+
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> dao = new DaoGeneric<Pessoa>();
 	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
-	
+
 	private IDaoPessoa iDaoPessoa = new IDaoPessoaImpl();
-	
+
 	public String salvar() {
 		pessoa = dao.merge(pessoa);
 		this.carregarPessoas();
-		
+
 		mostrarMsg("Cadastrado com Sucesso!");
-		
+
 		return "";
 	}
 
@@ -41,68 +47,88 @@ public class PessoaBean {
 		pessoa = new Pessoa();
 		return "";
 	}
-	
+
 	public String limpar() {
 		pessoa = new Pessoa();
 		return "";
 	}
-	
+
 	public String remover() {
 		dao.deletePorId(pessoa);
 		pessoa = new Pessoa();
-		
+
 		this.carregarPessoas();
-		
+
 		mostrarMsg("Removido com Sucesso!");
-		
+
 		return "";
 	}
-	
+
 	public String logar() {
-		
+
 		Pessoa p = iDaoPessoa.consultarUsuario(pessoa.getLogin(), pessoa.getSenha());
-				
-		if(p != null) {//Achou o Usuário
-			
+
+		if (p != null) {// Achou o Usuário
+
 			FacesContext context = FacesContext.getCurrentInstance();
 			ExternalContext externalContext = context.getExternalContext();
 			externalContext.getSessionMap().put("pessoaLogado", p);
-			
+
 			return "primeiraPagina.jsf";
-			
-		} 
-		
+
+		}
+
 		return "index.jsf";
 	}
-	
+
 	public boolean permiteAcesso(String acesso) {
-		
+
 		FacesContext context = FacesContext.getCurrentInstance();
 		ExternalContext externalContext = context.getExternalContext();
 		Pessoa p = (Pessoa) externalContext.getSessionMap().get("pessoaLogado");
-		
+
 		return p.getPerfilUser().equals(acesso);
-		
+
 	}
-	
+
 	@PostConstruct
 	public void carregarPessoas() {
 		pessoas = dao.getListEntity(Pessoa.class);
 	}
-	
+
 	private void mostrarMsg(String msg) {
-		
+
 		FacesContext context = FacesContext.getCurrentInstance();
 		FacesMessage message = new FacesMessage(msg);
 		context.addMessage(null, message);
 	}
-	
+
 	public void pesquisaCep( AjaxBehaviorEvent event) {
 		
-		System.out.println("Método pesquisa cep sendo chamado: " + pessoa.getCep());
-		
+		try {
+			URL url = new URL("https://viacep.com.br/ws/" + pessoa.getCep() + "/json/");
+			URLConnection conn = url.openConnection();
+			InputStream fluxoCep = conn.getInputStream();
+			BufferedReader br = new BufferedReader( new  InputStreamReader(fluxoCep, "UTF-8"));
+			
+			String cep = "";
+			StringBuffer jsonCep = new StringBuffer();
+			
+			while( (cep = br.readLine()) != null ) {
+				jsonCep.append(cep);
+			}
+			
+			System.out.println(jsonCep);
+			
+			
+		} catch( Exception ex ) {
+			
+			ex.printStackTrace();
+			mostrarMsg("Erro ao consultar o CEP");
+			
+		}
+			
 	}
-	
 
 	public Pessoa getPessoa() {
 		return pessoa;
